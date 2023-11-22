8c8504510aca8a9cbec4b5d958a85965ccc41231
firdawse <85738021+firdawse@users.noreply.github.com>
2023-11-22 15:35:55 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -40,9 +40,10 @@ class BaseModel():
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+            """String representation of the object."""
+            return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, str(self.__dict__)
+            )
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
