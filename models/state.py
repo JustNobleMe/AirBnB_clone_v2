34b8b38b6e5df7c665b24cfd78f5840cb7b43e15
Noble <84409340+JustNobleMe@users.noreply.github.com>
2023-11-23 16:53:37 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
+import models
 from models.city import City
-import os
 
+Base = declarative_base()
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    __tablename__ = 'states'
 
-    @property
-    def cities(self):
-        """getter attribute cities"""
-        from models import storage
-        list_cities = []
-        extracted_cities = storage.all(City).values()
-        for city in extracted_cities:
-            if self.id == city.state_id:
-                list_cities.append(city)
-        return list_cities
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Getter attribute in case of file storage"""
+            cities = models.storage.all(City)
+            cities_in_state = []
+            for city in cities.values():
+                if city.state_id == self.id:
+                    cities_in_state.append(city)
+            return cities_in_state
